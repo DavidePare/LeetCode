8ac1819f16fa86c5bbc1ee9ebcd964a6841ff55c
DavidePare <davipar@tiscali.it>
2022-02-12 19:05:57 +0100

    Create FindFirstUniqChar.cpp

---
+++ FindFirstUniqChar/FindFirstUniqChar.cpp
@@ -0,0 +1,20 @@
+// 57% 69%
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        bool flag=false;
+        for(int i=0; i<s.size();i++){
+            flag=false;
+            for(int j=0;j<s.size();j++){
+                if(s[i]==s[j] && i!=j){
+                    flag=true;
+                    break;
+                } 
+            }
+            if(!flag) return i;
+        }
+        
+        return -1;
+        
+    }
+};
